84c1120bc687b11a1e9f8774b475efb8e5e1c31f
YUSUKE <t.y.bsk@icloud.com>
2021-08-10 13:57:20 +0900

    study javascript

---
+++ app.js
@@ -0,0 +1,30 @@
+// let hello = 'Hello' + 'World';
+// alert(hello);
+
+// let i;
+// let num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert(num);
+
+// function foo(){
+//   let x = 'webcamp'
+//   let y = 'hello webcamp'
+//   console.log(x)
+//   console.log(y)
+// }
+
+// foo()
+
+var str = "webcamp"
+
+function foo() { 
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
+}
+
+foo()
