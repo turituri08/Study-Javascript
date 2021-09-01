6f44e8cb99bc751438adb7f8bedfbea9d42a4731
YUSUKE <t.y.bsk@icloud.com>
2021-09-01 22:43:36 +0900

    ブロック崩し

--- input.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('form').onsubmit = function(event){
-  event.preventDefault
-  const search = document.getElementById('form').word.value;
-  document.getElementById('output').textContent = `「${search}」の検索中`;
-};
--- math_pi.js
+++
@@ -1,6 +0,0 @@
-function point(num, digit) {
-  const mover = 10 ** digit;
-  return Math.floor(num * mover) / mover;
-}
-
-document.getElementById('output').textContent = point(Math.PI, 2);
